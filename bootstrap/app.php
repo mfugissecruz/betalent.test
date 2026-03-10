e79a7d76c1aef1ba45956d5c0b2054ac9200c2aa
Marcelo Cruz <mfugissecruz@gmail.com>
2026-03-10 00:48:17 -0300

    feat: update PHP version to 8.5.3, add Laravel Sanctum, and create personal access tokens migration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,14 @@
 <?php
 
+declare(strict_types = 1);
+
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\{Exceptions, Middleware};
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
